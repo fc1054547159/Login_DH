8b40017c5388e2383ab5299d5d3160565ccb28b6
fc1054547159 <fabiocruzlozano@gmail.com>
2023-02-09 22:41:48 -0500

    se arreglo el login para que solo aparezca el boton para iniciar sesion, y se oculto para que no  aparezca cuando ya se ha iniciado la sesion, es decir solo se vea el boton le logout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
 
+import { useAuth0 } from '@auth0/auth0-react';
+
 function App() {
+  const {isAuthenticated} = useAuth0 ()
+
   return (
     <div className="App">
       <h1>aplicacion</h1>
-      <LoginButton />
-      <LogoutButton/>
+      {isAuthenticated ? <LogoutButton/> : <LoginButton />}
       <Profile />
     </div>
   );
