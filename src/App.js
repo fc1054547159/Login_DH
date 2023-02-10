ebfad20ed9476bdbec0c6dd29fa1e3cda17381c0
fc1054547159 <fabiocruzlozano@gmail.com>
2023-02-09 22:46:27 -0500

    se creo un loading que dice Ingresando a su perfil...

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Profile from './components/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  const {isAuthenticated} = useAuth0 ()
+  const {isAuthenticated, isLoading } = useAuth0 ()
+
+  if (isLoading) return <h1>Ingresando a su perfil...</h1>
 
   return (
     <div className="App">
