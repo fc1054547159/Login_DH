30a612629f19853cd32f65e5491ff4142e6e68b3
fc1054547159 <fabiocruzlozano@gmail.com>
2023-02-09 20:30:25 -0500

    creando Login con auth0

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Auth0Provider>
+    <App />
+  </Auth0Provider>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
